17f163f41d543a153afbac987759f8106bceaf38
aryan-212 <aryananandxic07@gmail.com>
2024-02-24 08:44:52 +0530

    BufferClose added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<leader>r", ":RunCode<CR>", { noremap = true, silent = false })
+vim.keymap.set("n", "<leader>q", ":bd<CR>", { noremap = true, silent = false })
