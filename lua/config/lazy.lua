8a2d22bc54f8a5c8007ee3bfe2e627d1ff2238dc
aryan-212 <allalonearyan@gmail.com>
2025-05-18 12:12:17 +0530

    xtra plugins

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -48,6 +48,15 @@ require("lazy").setup({
         -- configuration goes here
       },
     },
+    -- {
+    --   "rachartier/tiny-inline-diagnostic.nvim",
+    --   event = "VeryLazy", -- Or `LspAttach`
+    --   priority = 1000, -- needs to be loaded in first
+    --   config = function()
+    --     require("tiny-inline-diagnostic").setup()
+    --     vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+    --   end,
+    -- },
     {
       "mrcjkb/rustaceanvim",
       version = "^6", -- Recommended
